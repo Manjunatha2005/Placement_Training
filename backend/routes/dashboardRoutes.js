8eb722616faaf85a28db075576ffe4021ee5624a
Manjunatha s <manjunatha2055@gamil.com>
2026-04-16 13:53:35 +0530

    commit

--- backend/routes/dashboardRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getStudentDashboard, getAdminDashboard } = require('../controllers/dashboardController');
-const { protect, adminOnly } = require('../middleware/authMiddleware');
-router.get('/student', protect, getStudentDashboard);
-router.get('/admin', protect, adminOnly, getAdminDashboard);
-module.exports = router;
